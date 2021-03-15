8bbfbb43e98ab9f73ba6dddebc489a9e44ef2d04
kubitckiy_bogdan <bk@albedo.dev>
2021-03-15 18:17:40 +0200

    add webpack

---
+++ build/webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        bundle: './src/app.js'
+    } ,
+    output: {
+        path: path.resolve(__dirname, '../dist')
+    },
+    devtool: "source-map",
+    devServer: {
+        port: 3000,
+        open: true
+    },
+    module: {
+        rules: [
+            { test: /\.handlebars$/, loader: "handlebars-loader" },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'static/',
+                            useRelativePath: true,
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            optipng: {
+                                enabled: true,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    },
+                ]
+            }
+        ]
+    },
+    plugins: [
+        /** Since Webpack 4 */
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                handlebarsLoader: {}
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name]-styles.css",
+            chunkFilename: "[id].css"
+        }),
+        new HtmlWebpackPlugin({
+            title: 'FitApp',
+            template: './src/index.handlebars'
+        })
+    ]
+};
