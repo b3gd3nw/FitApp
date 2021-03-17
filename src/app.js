bc7c93d758f75976bddf9de1ca48b1a3434c20c0
Богдан <bog@pop-os.localdomain>
2021-03-17 18:29:38 +0200

    finish design|add adaptivity 90%

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 import './main.scss';
+require('./assets/images/FitApp-logo.png');
+require('./assets/images/App_Icon.png');
+require('./assets/images/iPhone_mock_up.png');
+require('./assets/images/iPhone_5C_Mock_Up_Features.png');
+require('./assets/images/plan-iphones.png');
+require('./assets/images/features-item-bell.png');
+require('./assets/images/features-item-heart.png');
+require('./assets/images/features-item-graph.png');
+require('./assets/images/features-item-list.png');
+require('./assets/images/features-item-calendar.png');
+require('./assets/images/apple.png');
+require('./assets/images/salate.png');
+require('./assets/images/sandwich.png');
+
